1bd724c229313aeb9675a2efc62153885f6da232
smlatic <edin.smlatic@gmail.com>
2023-05-17 00:42:31 +0200

    update

--- update_database.py
+++ update_database.py
@@ -2,29 +2,29 @@ from sqlalchemy import create_engine, text
 import pandas as pd
 import getpass
 
-# Ask for login credentials, database name, and book title
+# Login credentials, database name, and book title
 username = input("Please enter your username (leave blank): ")
 password = getpass.getpass("Please enter your password (leave blank): ")
 server_name = input("Please enter your server name(localhost): ")
 database_name = input("Please enter your database name (my is 'bokhandel'): ")
 book_title = input("Please enter the book title you want to search for (example:Book Title 1): ")
 
-# Create a connection string
+# Connection string
 connection_string = f'mssql+pyodbc://{username}:{password}@{server_name}/{database_name}?driver=ODBC+Driver+17+for+SQL+Server'
 
-# Create a database engine
+# Database engine
 engine = create_engine(connection_string)
 
 # Connect to the database
 conn = engine.connect()
 
-# Define a function that performs a full-text search for a book title
+# Performs a full-text search for a book title
 def search_book_title(title):
     query = text("SELECT * FROM Böcker WHERE Titel LIKE :title")
     result = conn.execute(query, title=f'%{title}%')
     return pd.DataFrame(result.fetchall(), columns=result.keys())
 
-# Define a function that gets the number of copies of each book in each store
+# Gets the number of copies of each book in each store
 def get_book_copies_in_stores():
     query = text("SELECT ButikID, ISBN13, Antal FROM LagerSaldo")
     result = conn.execute(query)
